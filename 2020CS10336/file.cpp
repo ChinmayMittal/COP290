ee630e388ae272980c9185827dcd465d63ee43a8
ChinmayMittal <chinmay3102@gmail.com>
2022-02-08 17:02:27 +0530

    MKL FUNCTIONALITY ADDED

--- 2020CS10336/file.cpp
+++ 2020CS10336/file.cpp
@@ -10,12 +10,21 @@
 #include"matrixAlgebra.cpp"
 #include"validator.h"
 #include"timing.h"
+#include"matGen.cpp"
+#include"mkl.h"
 
 using namespace std ; 
+using namespace std::chrono;
 
 int main( int argc , const char * argv [] ){
 
-
+    // vector<vector<float>>a(100,vector<float>(100)) ;
+    // matGen(a,0,10) ;
+    // write(a , "matrix.txt") ;  
+    // matGen(a,0,10) ; 
+    // write(a,"weight.txt")  ; 
+    // matGen(a,0,10) ; 
+    // write(a,"bias.txt") ; 
     string task(argv[1]); // stores the name of the function to be called ( fullyconnected/activation/pooling/probability)
     
     try{
@@ -25,19 +34,47 @@ int main( int argc , const char * argv [] ){
             if( argc < 6 ) {
                 throw ( "fully connected requires arguments in the form of fullyconnected (input_matrix_filename) (weight_matrix_filename) (bias_matrix_filename) (output_matrix_filename)") ;
              }
+            string inputMatFileName( argv[2]) , weightMatFileName( argv[3]) , biasMatFileName( argv[4]) , outputFileName( argv[5]) ;
+            // get input, weight and bias matrices  
+            vector<vector<float>>mat = read(inputMatFileName), weights = read( weightMatFileName) , bias = read(biasMatFileName) ; 
+            if(argc == 6 ){
+                // naive implementation 
+                auto t1 = get_time() ; 
+                vector<vector<float>> mulMat = mul( mat , weights) ; // matrix multiplication 
+                auto t2 = get_time() ; 
+                printTimeDuration("MATMUL NAIVE:" , t1 , t2 )  ; 
+                vector<vector<float>> ans = add( mulMat , bias) ;  // matrix addition 
+                write( ans , outputFileName ) ; // output to filename
+            }else{
+                // optimized implementation 
+                string optimization(argv[6]) ;
+                if(optimization == "mkl"){
+                     // Matrices
+                     double *A, *B, *C , *outputMatrix ;
+                     A = (double *)mkl_malloc(mat.size() * mat[0].size() * sizeof(double), 64);
+                     B = (double *)mkl_malloc(weights.size() * weights[0].size() * sizeof(double), 64);
+                     C = (double *)mkl_malloc(bias.size() * bias[0].size() * sizeof(double), 64);
+                     vec2Arr( A , mat ) ; vec2Arr( B , weights ) , vec2Arr(C,weights) ; 
+                     // Scale factors
+                     double alpha, beta;
+                     alpha = 1.0;
+                     beta = 1.0;
+                    auto mkl_t1 = get_time();
+                    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, mat.size() , weights[0].size() , mat[0].size(),
+                            alpha, A, mat[0].size() , B, weights[0].size(), beta, C, weights[0].size());
+                    auto mkl_t2 = get_time();                     
+                    printTimeDuration("MATMUL MKL:" , mkl_t1 , mkl_t2 )  ; 
+                    vector<vector<float>> ans = arr2Vec(C,bias.size(),bias[0].size());
+                    write( ans , outputFileName) ; 
+                }else if( optimization == "openblas"){
 
-             string inputMatFileName( argv[2]) , weightMatFileName( argv[3]) , biasMatFileName( argv[4]) , outputFileName( argv[5]) ; 
-             // get input, weight and bias matrices 
-             
-             vector<vector<float>>mat = read(inputMatFileName), weights = read( weightMatFileName) , bias = read(biasMatFileName);
-             auto t1 = get_time() ; 
-             vector<vector<float>> mulMat = mul( mat , weights) ; // matrix multiplication 
-             auto t2 = get_time() ; 
-             printTimeDuration("MATMUL" , t1 , t2 )  ; 
-             vector<vector<float>> ans = add( mulMat , bias) ;  // matrix addition 
-             write( ans , outputFileName ) ; // output to filename
+                }else if( optimization == "pthread"){
 
+                }else{
+                    throw ( optimization + " is not supported use mkl|openblas|pthread") ; 
+                }
 
+            }
         }else if( task == "activation"){
 
             if(argc < 5  ){
@@ -120,5 +157,35 @@ int main( int argc , const char * argv [] ){
     }
 
 
+
+    // Matrix dimensions
+    int m, n, k;
+    m = 2000;
+    k = 200;
+    n = 1000;
+
+
+
+    // Allocate 64-byte-aligned memory
+    // A is a "m x k" matrix
+//     A = (double *)mkl_malloc(m * k * sizeof(double), 64);
+//     // B is a "k x n" matrix
+//     B = (double *)mkl_malloc(k * n * sizeof(double), 64);
+//     // C is a "m x n" matrix
+//     C = (double *)mkl_malloc(m * n * sizeof(double), 64);
+//         // Initialize the matrices
+//     init_matrix<double>(A, m, k);
+//     init_matrix<double>(B, k, n);
+//     // Initialize the result matrix to 0s
+//     init_matrix<double>(C, m, n, 0);
+//     auto mkl_t1 = get_time();
+//     cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, m, n, k,
+//             alpha, A, k, B, n, beta, C, n);
+//     auto mkl_t2 = get_time();
+
+//     // Compute the execution time
+//     auto mkl_time_span =
+//         duration_cast<duration<double>>(mkl_t2 - mkl_t1);
+//     cout << "Elapsed time MKL: " << mkl_time_span.count() << " s" << endl;
     return 0 ; 
 }
---
+++ 2020CS10336/matGen.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<vector>
+#include <cstdlib> 
+#include <ctime> 
+#include <stdlib.h>
+#include<mkl.h>
+
+using namespace std ; 
+
+void matGen( vector<vector<float>>&mat , float lrange = 0 , float rrange = 1){
+
+    srand(static_cast<unsigned int>(std::time(nullptr)));
+
+    int m = mat.size() , n = mat[0].size() ; 
+    for( int i = 0 ; i <  m ; i++ ){
+        for( int j = 0 ; j < n ; j ++ ){
+            mat[i][j] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX/(rrange-lrange));
+        }
+    }
+
+}
+
+// void init_matrix(float *a, int m, int n, float value = 1){
+//     for(int i = 0; i < m * n; i++){
+//         if(value == 1){
+//             a[i] = (float)(rand() % 100);
+//         }else{
+//             a[i] = 0;
+//         }
+//     }
+// }
+template <class T>
+void init_matrix(T *a, int m, int n, T value = 1){
+    for(int i = 0; i < m * n; i++){
+        if(value == 1){
+            a[i] = (T)(rand() % 100);
+        }else{
+            a[i] = 0;
+        }
+    }
+}
+
+void vec2Arr( double* A , vector<vector<float>>&mat ) { 
+    int m = mat.size() , n = mat[0].size() ;
+     
+    for( int i = 0 ; i < m ; i ++ ){
+        for( int j = 0 ; j < n ; j ++ ){
+            A[n*i+j] = (double)mat[i][j] ; 
+        }
+    }
+}
+
+vector<vector<float>> arr2Vec( double * A , int m , int n ) {
+
+    vector<vector<float>>ans( m , vector<float>(n,0.0f)) ; 
+    for( int i = 0 ; i < m*n ; i ++ ){
+        ans[i/n][i%n] = A[i] ; 
+    } 
+    return ans ; 
+}
