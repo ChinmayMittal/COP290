de78b1b88dc2a0a16f9bf2f33b69ea73b1a15e9f
TanishTuteja <tanishtuteja@gmail.com>
2022-02-21 19:23:33 +0530

    Added more sizes + increased runs

--- 2020CS10336_2020CS10398/latency_consts.h
+++ 2020CS10336_2020CS10398/latency_consts.h
@@ -7,12 +7,20 @@
 const std::vector<std::string> DEFAULT_FILES{
     "mat10x10_0.txt", "mat10x10_1.txt", "mat10x10_2.txt",
     "mat20x20_0.txt", "mat20x20_1.txt", "mat20x20_2.txt",
+    "mat30x30_0.txt", "mat30x30_1.txt", "mat30x30_2.txt",
     "mat50x50_0.txt", "mat50x50_1.txt", "mat50x50_2.txt",
+    "mat75x75_0.txt", "mat75x75_1.txt", "mat75x75_2.txt",
     "mat100x100_0.txt", "mat100x100_1.txt", "mat100x100_2.txt",
-    "mat200x200_0.txt", "mat200x200_1.txt", "mat200x200_2.txt"};
+    "mat125x125_0.txt", "mat125x125_1.txt", "mat125x125_2.txt",
+    "mat150x150_0.txt", "mat150x150_1.txt", "mat150x150_2.txt",
+    "mat200x200_0.txt", "mat200x200_1.txt", "mat200x200_2.txt",
+    "mat250x250_0.txt", "mat250x250_1.txt", "mat250x250_2.txt",
+    "mat300x300_0.txt", "mat300x300_1.txt", "mat300x300_2.txt",
+    "mat400x400_0.txt", "mat400x400_1.txt", "mat400x400_2.txt",
+    "mat500x500_0.txt", "mat500x500_1.txt", "mat500x500_2.txt"};
 
 // Default x-axis labels to be used for latency plots.
-const std::vector<std::string> DEFAULT_LABELS{"10x10", "20x20", "50x50", "100x100", "200x200"};
+const std::vector<std::string> DEFAULT_LABELS{"10x10", "20x20", "30x30", "50x50", "75x75", "100x100", "125x125", "150x150", "200x200", "250x250", "300x300", "400x400", "500x500"};
 
 // Files to which computed latencies will be output. These are used by gnuplot to generate the plots.
 const std::string OUT_NAIVE = "naive.dat";
@@ -21,4 +29,4 @@ const std::string OUT_OPENBLAS = "openblas.dat";
 const std::string OUT_MKL = "mkl.dat";
 
 // Number of runs per matrix to compute the mean latency.
-const int RUNS = 100;
+const int RUNS = 500;
--- 2020CS10336_2020CS10398/test/run_tests.sh
+++ 2020CS10336_2020CS10398/test/run_tests.sh
@@ -10,9 +10,17 @@ make
 
 ./randomMat.out mult 3 10
 ./randomMat.out mult 3 20
+./randomMat.out mult 3 30
 ./randomMat.out mult 3 50
+./randomMat.out mult 3 75
 ./randomMat.out mult 3 100
+./randomMat.out mult 3 125
+./randomMat.out mult 3 150
 ./randomMat.out mult 3 200
+./randomMat.out mult 3 250
+./randomMat.out mult 3 300
+./randomMat.out mult 3 400
+./randomMat.out mult 3 500
 
 ./yourcode.out calclatency
 ./openblas_mm.out calclatency
